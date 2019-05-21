7cad606d62e4a086b50758f7df7bfd8db8d8332f
dieguischa <dfchacona@unal.edu.co>
2019-05-21 17:52:05 -0500

    Protecting users

---
+++ app/serializers/user_serializer.rb
@@ -0,0 +1,17 @@
+class UserSerializer < ActiveModel::Serializer
+    attributes :user_name, :e_mail, :last_connection, :friends
+
+    def friends
+        ret=[] 
+        friends= Friend.where("user_id1 = ? ",object.id)
+        friends.each do |friend|
+            ret.push(User.getUserNameById(friend.user_id2))
+        end
+
+        friends = Friend.where("user_id2 = ? ",object.id)
+        friends.each do |friend|
+            ret.push(User.getUserNameById(friend.user_id1))
+        end
+        return ret
+    end
+end
